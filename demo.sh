b37f3f3122abb0e4fe56d1137e743c31eaa7ed9b
zhanghming <zhanghm_1995@qq.com>
2022-04-15 14:15:00 +0800

    update for demo

--- demo.sh
+++ demo.sh
@@ -1,6 +1,7 @@
 set -x
 
-checkpoint="work_dir/train_face_3dmm_expression_mouth_mask_paper/lightning_logs/version_1/checkpoints_test/epoch=86-step=45239.ckpt"
+# checkpoint="work_dir/train_face_3dmm_expression_mouth_mask_paper/lightning_logs/version_1/checkpoints_test/epoch=86-step=45239.ckpt"
+checkpoint="work_dir/train_face_3dmm_expression_mouth_mask_paper/lightning_logs/version_0/checkpoints_test/epoch=70-step=36919.ckpt"
 python demo.py --cfg config/demo.yaml --test_mode \
                --checkpoint ${checkpoint} \
                --checkpoint_dir work_dir/demo
--- visualizer/main_render_pred_results.sh
+++ visualizer/main_render_pred_results.sh
@@ -1,20 +1,10 @@
 ### You can use this script to render the results of the prediction.
 set -x
 
-generated_vertex_path="/home/zhanghm/Research/V100/TalkingFaceFormer/work_dir/demo/lightning_logs/version_0/vis/train/WDA_KimSchrier_000_000.npy"
+generated_vertex_path="/home/zhanghm/Research/V100/TalkingFaceFormer/work_dir/demo/lightning_logs/version_4/vis/train/WRA_KellyAyotte_000_000.npy"
 
-python main_render_pred_results.py --data_root ../data/HDTF_face3dmmformer/train \
-                                   --video_name WDA_KimSchrier_000 \
-                                   --output_root ../testing/demo \
+python main_render_pred_results.py --data_root ../data/HDTF_face3dmmformer/val \
+                                   --video_name WRA_KellyAyotte_000 \
+                                   --output_root ../test_dir/demo_audio_chinese_tofu_PPE \
                                    --gen_vertex_path ${generated_vertex_path} \
                                    --need_pose
-
-
-##### Old version ###########
-# generated_vertex_path="/home/haimingzhang/Research/Github/FaceFormer/work_dir/train_face_3dmm_expression_mouth_mask_paper/test/lightning_logs/version_1/vis/train/WDA_BarackObama_000_000.npy"
-
-# python main_render_pred_results.py --data_root ../data/HDTF_preprocessed \
-#                                    --video_name WDA_BarackObama_000_000 \
-#                                    --output_root ../testing/paper_val_WDA_BarackObama_000_000 \
-#                                    --gen_vertex_path ${generated_vertex_path} \
-#                                    --need_pose
